c559856b6d7a1e78a6fb212280e24ff47458c4c3
Sky <diogo_goncalves2004@hotmail.com>
2024-10-10 14:01:41 +0000

    feat: adding initial tests and places to add tests

---
+++ runtests_intermediate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+let total=0;
+let correct=0;
+
+for x in ./tests_intermediate_*/*.in; do
+    if [ -e ${x%.in}.import ]; then
+        java -cp ../lib/po-uilib.jar:.. -Dimport=${x%.in}.import -Din=$x -DwriteInput=true -Dout=${x%.in}.outhyp hva.app.App;
+    else
+        java -cp ../lib/po-uilib.jar:.. -Din=$x -DwriteInput=true -Dout=${x%.in}.outhyp hva.app.App;
+    fi
+
+    diff -cwB ${x%.in}.out ${x%.in}.outhyp > ${x%.in}.diff ;
+    if [ -s ${x%.in}.diff ]; then
+        echo -n "F"
+        failures=$failures"Fail: $x: See file ${x%.in}.diff\n" ;
+    else
+        let correct++;
+        echo -n "."
+        rm -f ${x%.in}.diff ${x%.in}.outhyp ; 
+    fi
+    let total++;
+done
+
+rm -f saved*
+let res=100*$correct/$total
+echo ""
+echo "Total Tests = " $total
+echo "Passed = " $res"%"
+printf "$failures"
+echo "Done."
+
